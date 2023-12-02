fa100e352fb1260c2d1470058095f8938a7a0f7a
JustAlittleWolf <jakobi5000@gmail.com>
2023-12-02 06:30:06 +0100

    Implement day 2

--- src/main/java/me/wolfii/Main.java
+++ src/main/java/me/wolfii/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         AutoSolver.setSessionToken(EnvReader.get("ADVENT_OF_CODE_SESSION_TOKEN"));
-        AutoSolver.solve(1, SolveSelect.BOTH, SolveEnvironment.TEST_THEN_PROD);
+
+        AutoSolver.solve(2, SolveSelect.BOTH, SolveEnvironment.TEST_THEN_PROD);
     }
 }
--- src/main/java/me/wolfii/implementations/day2/Day2.java
+++ src/main/java/me/wolfii/implementations/day2/Day2.java
@@ -6,10 +6,60 @@ import java.util.List;
 
 public class Day2 implements Solution {
     public void solveFirst(List<String> lines) {
+        int sum = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            CubeSet cubes = this.getCubesOfLine(lines.get(i));
+            if (cubes.red <= 12 && cubes.green <= 13 && cubes.blue <= 14) {
+                sum += i + 1;
+            }
+        }
 
+
+        System.out.println("Part 1: " + sum);
     }
 
     public void solveSecond(List<String> lines) {
+        int sum = 0;
 
+        for (String line : lines) {
+            CubeSet cubes = this.getCubesOfLine(line);
+            sum += (cubes.red * cubes.blue * cubes.green);
+        }
+
+        System.out.println("Part 2: " + sum);
+    }
+
+    private CubeSet getCubesOfLine(String line) {
+        CubeSet cubes = new CubeSet(0, 0, 0);
+
+        String dataPart = line.split(":")[1];
+        for (String match : dataPart.split(";")) {
+            for (String cube : match.split(",")) {
+                cube = cube.trim();
+                if (cube.endsWith("red")) {
+                    cubes.red = Math.max(Integer.parseInt(cube.split(" ")[0]), cubes.red);
+                }
+                if (cube.endsWith("blue")) {
+                    cubes.blue = Math.max(cubes.blue, Integer.parseInt(cube.split(" ")[0]));
+                }
+                if (cube.endsWith("green")) {
+                    cubes.green = Math.max(Integer.parseInt(cube.split(" ")[0]), cubes.green);
+                }
+            }
+        }
+
+        return cubes;
+    }
+
+    private static class CubeSet {
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        public CubeSet(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
     }
 }
