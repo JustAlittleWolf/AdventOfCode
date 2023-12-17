1d3995d52822773230c9b22ff22fa96f28438590
JustAlittleWolf <jakobi5000@gmail.com>
2023-12-17 10:27:29 +0100

    Optimize Day 17
    Changed from a HashSet to an int[][][] which stores the visited directions and distances bitwise.
    Performance improvement ~6-10x

--- src/main/java/me/wolfii/implementations/day17/Day17.java
+++ src/main/java/me/wolfii/implementations/day17/Day17.java
@@ -40,7 +40,7 @@ public class Day17 implements Solution {
         int height = heatLosses[0].length;
         Vec2 target = new Vec2(width - 1, height - 1);
 
-        Set<DirectionalVector> visited = new HashSet<>();
+        int[][][] visitedBitmap = new int[width][height][4];
         int heatLoss = 0;
         List<MemoryVector> processNext = new ArrayList<>();
         processNext.add(new MemoryVector(new Vec2(1, 0), Direction.EAST, 1, 0));
@@ -51,35 +51,43 @@ public class Day17 implements Solution {
             heatLoss++;
             List<MemoryVector> nextProcessNext = new ArrayList<>();
             for (MemoryVector memoryVector : processNext) {
-                if (memoryVector.position.x() < 0 || memoryVector.position.x() >= width) continue;
-                if (memoryVector.position.y() < 0 || memoryVector.position.y() >= height) continue;
-                DirectionalVector directionalVector = new DirectionalVector(memoryVector.position, memoryVector.direction, memoryVector.distance);
-                if (visited.contains(directionalVector)) continue;
-                int currentHeatLoss = memoryVector.heatLoss + heatLosses[memoryVector.position().x()][memoryVector.position().y()];
+                Vec2 position = memoryVector.position();
+                Direction direction = memoryVector.direction();
+                if (position.x() < 0 || position.x() >= width) continue;
+                if (position.y() < 0 || position.y() >= height) continue;
+                if ((visitedBitmap[position.x()][position.y()][getIndex(direction)] & (1 << memoryVector.distance)) != 0)
+                    continue;
+                int currentHeatLoss = memoryVector.heatLoss + heatLosses[position.x()][position.y()];
                 if (currentHeatLoss > heatLoss) {
                     nextProcessNext.add(memoryVector);
                     continue;
                 }
-                visited.add(directionalVector);
-                if (memoryVector.position.equals(target)) break outer;
-                MemoryVector straight = new MemoryVector(memoryVector.position.plus(memoryVector.direction.vec2()), memoryVector.direction, memoryVector.distance + 1, currentHeatLoss);
+                visitedBitmap[position.x()][position.y()][getIndex(direction)] |= 1 << memoryVector.distance;
+                if (position.equals(target)) break outer;
+                MemoryVector straight = new MemoryVector(position.plus(direction.vec2()), direction, memoryVector.distance + 1, currentHeatLoss);
                 if (straight.distance <= maxStraight) nextProcessNext.add(straight);
                 if (straight.distance <= minTurn) continue;
-                Direction left = memoryVector.direction.turnLeft();
-                nextProcessNext.add(new MemoryVector(memoryVector.position.plus(left.vec2()), left, 1, currentHeatLoss));
-                Direction right = memoryVector.direction.turnRight();
-                nextProcessNext.add(new MemoryVector(memoryVector.position.plus(right.vec2()), right, 1, currentHeatLoss));
+                Direction left = direction.turnLeft();
+                nextProcessNext.add(new MemoryVector(position.plus(left.vec2()), left, 1, currentHeatLoss));
+                Direction right = direction.turnRight();
+                nextProcessNext.add(new MemoryVector(position.plus(right.vec2()), right, 1, currentHeatLoss));
             }
-
             processNext = nextProcessNext;
-
         }
         return heatLoss;
+    }
+
+    private int getIndex(Direction direction) {
+        return switch (direction) {
+            case NORTH -> 0;
+            case SOUTH -> 1;
+            case EAST -> 2;
+            case WEST -> 3;
+            default -> -1;
+        };
     }
 
 
     private record MemoryVector(Vec2 position, Direction direction, int distance, int heatLoss) {
     }
-
-    private record DirectionalVector(Vec2 position, Direction direction, int distance){}
 }
