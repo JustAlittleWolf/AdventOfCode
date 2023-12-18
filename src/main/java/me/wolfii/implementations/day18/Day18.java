4b3ae2d08b88dbfd57162a84a055311d7604ed21
JustAlittleWolf <jakobi5000@gmail.com>
2023-12-18 17:48:00 +0100

    Implement Day 18 Part 2

--- src/main/java/me/wolfii/implementations/day18/Day18.java
+++ src/main/java/me/wolfii/implementations/day18/Day18.java
@@ -8,51 +8,56 @@ import java.util.*;
 
 public class Day18 implements Solution {
     public void solveFirst(List<String> lines) {
-        HashSet<Vec2> dugSpaces = new HashSet<>();
+        List<Vec2> corners = getCorners(lines, false);
+        long area = getArea(corners);
+        long circumference = getCircumference(corners);
 
-        Vec2 position = new Vec2(0, 0);
-        int minY = Integer.MAX_VALUE;
-        int maxY = 0;
-        int minX = Integer.MAX_VALUE;
-        int maxX = 0;
-        dugSpaces.add(position);
-        for (String line : lines) {
-            String[] parts = line.split(" ");
-
-            Direction direction = switch (parts[0]) {
-                case "R" -> Direction.EAST;
-                case "L" -> Direction.WEST;
-                case "U" -> Direction.NORTH;
-                case "D" -> Direction.SOUTH;
-                default -> Direction.NONE;
-            };
-            int count = Integer.parseInt(parts[1]);
-
-            for (int i = 0; i < count; i++) {
-                position = position.plus(direction.vec2());
-                minY = Math.min(position.y(), minY);
-                maxY = Math.max(position.y(), maxY);
-                minX = Math.min(position.x(), minX);
-                maxX = Math.max(position.x(), maxX);
-                dugSpaces.add(position);
-            }
-        }
-
-        for (int y = minY; y <= maxY; y++) {
-            int passThroughCount = 0;
-            for (int x = minX; x <= maxX; x++) {
-                if (dugSpaces.contains(new Vec2(x, y)) && dugSpaces.contains(new Vec2(x, y + 1))) {
-                    passThroughCount++;
-                    continue;
-                }
-                if (passThroughCount % 2 == 1) dugSpaces.add(new Vec2(x, y));
-
-            }
-        }
-
-        System.out.println("Part 1: " + dugSpaces.size());
+        System.out.println("Part 1: " + (area + (circumference / 2) + 1));
     }
 
     public void solveSecond(List<String> lines) {
+        List<Vec2> corners = getCorners(lines, true);
+        long area = getArea(corners);
+        long circumference = getCircumference(corners);
+
+        System.out.println("Part 2: " + (area + (circumference / 2) + 1));
+    }
+
+    private List<Vec2> getCorners(List<String> lines, boolean hexadecimal) {
+        List<Vec2> corners = new ArrayList<>(List.of(new Vec2(0, 0)));
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+            int distance = hexadecimal ? Integer.parseInt(parts[2].substring(2, 7), 16) : Integer.parseInt(parts[1]);
+            Direction direction = switch (hexadecimal ? parts[2].charAt(7) : parts[0].charAt(0)) {
+                case 'R', '0' -> Direction.EAST;
+                case 'D', '1' -> Direction.SOUTH;
+                case 'L', '2' -> Direction.WEST;
+                case 'U', '3' -> Direction.NORTH;
+                default -> Direction.NONE;
+            };
+            Vec2 lastCorner = corners.get(corners.size() - 1);
+            corners.add(lastCorner.plus(direction.vec2().multiply(distance)));
+        }
+        return corners;
+    }
+
+    private long getCircumference(List<Vec2> corners) {
+        long circumference = 0;
+        for (int i = 0; i < corners.size() - 1; i++) {
+            Vec2 first = corners.get(i);
+            Vec2 second = corners.get(i + 1);
+            circumference += Math.abs(first.x() - second.x()) + Math.abs(first.y() - second.y());
+        }
+        return circumference;
+    }
+
+    private long getArea(List<Vec2> corners) {
+        long area = 0;
+        for (int i = 1; i < corners.size() - 1; i++) {
+            Vec2 first = corners.get(i);
+            Vec2 second = corners.get(i + 1);
+            area += (long) first.x() * second.y() - (long) second.x() * first.y();
+        }
+        return Math.abs(area / 2);
     }
 }
