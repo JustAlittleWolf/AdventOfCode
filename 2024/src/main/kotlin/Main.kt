48d454e9bdacdde09d326c5b3deee6a97f78e494
JustAlittleWolf <jakobi5000@gmail.com>
2024-12-01 08:19:23 +0100

    feat: Day1

--- 2024/src/main/kotlin/Main.kt
+++ 2024/src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 package me.wolfii
 
 import me.wolfii.automation.Input
+import me.wolfii.automation.Part
 import me.wolfii.automation.SolveOptions
 import me.wolfii.automation.Solver
 
 fun main() {
     Solver.solve(
         SolveOptions(
+            part = Part.BOTH,
             input = Input.BOTH
         )
     )
--- 2024/src/main/kotlin/year2024/day1/Day1.kt
+++ 2024/src/main/kotlin/year2024/day1/Day1.kt
@@ -1,13 +1,35 @@
 package me.wolfii.year2024.day1
 
 import me.wolfii.automation.Solution
+import java.util.*
+import kotlin.math.abs
 
 class Day1 : Solution {
-    override fun solveFirst(lines: List<String>) {
-        TODO()
+    override fun solveFirst(lines: List<String>): Any? {
+        val leftList = PriorityQueue<Int>()
+        val rightList = PriorityQueue<Int>()
+
+        lines.forEach { line ->
+            leftList.add(line.firstId())
+            rightList.add(line.secondId())
+        }
+
+        var sum = 0
+        while (leftList.isNotEmpty()) {
+            sum += abs(leftList.poll() - rightList.poll())
+        }
+        return sum
     }
 
-    override fun solveSecond(lines: List<String>) {
-        TODO()
+    override fun solveSecond(lines: List<String>): Any? {
+        val keys = HashSet<Int>()
+        lines.forEach { line -> keys.add(line.firstId()) }
+        return lines.sumOf { line ->
+            val value = line.secondId()
+            if (keys.contains(value)) return@sumOf value else 0
+        }
     }
+
+    private fun String.firstId() = substringBefore(' ').toInt()
+    private fun String.secondId() = substringAfterLast(' ').toInt()
 }
