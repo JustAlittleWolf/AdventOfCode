a7da34f6aa1675f8a4d064f10496689c185cd837
JustAlittleWolf <jakobi5000@gmail.com>
2024-12-03 21:29:44 +0100

    fix: Day3 minor cleanup

--- 2024/src/main/kotlin/year2024/day3/Day3.kt
+++ 2024/src/main/kotlin/year2024/day3/Day3.kt
@@ -4,16 +4,16 @@ import me.wolfii.automation.Solution
 
 class Day3 : Solution {
     override fun solveFirst(lines: List<String>): Int {
-        return lines.joinToString("").sumOfMultiplications()
+        return lines.joinToString().sumOfMultiplications()
     }
 
     override fun solveSecond(lines: List<String>): Int {
-        return lines.joinToString("").split("do()").map { it.split("don't()").first() }.sumOf { valid ->
+        return lines.joinToString().split("do()").map { it.split("don't()", limit = 2).first() }.sumOf { valid ->
             valid.sumOfMultiplications()
         }
     }
 
-    private fun String.sumOfMultiplications() = "mul\\(\\d+,\\d+\\)".toRegex().findAll(this).sumOf { result ->
-        result.value.removePrefix("mul(").removeSuffix(")").split(",").map(String::toInt).reduce { a, b -> a * b }
+    private fun String.sumOfMultiplications() = """mul\(\d{1,3},\d{1,3}\)""".toRegex().findAll(this).sumOf { result ->
+        result.value.removeSurrounding("mul(", ")").split(",").map(String::toInt).let { it[0] * it[1] }
     }
 }
