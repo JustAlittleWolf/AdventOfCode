296b94f5250ae58a9bf2d77cc905fabf1597aed7
JustAlittleWolf <jakobi5000@gmail.com>
2024-12-07 09:31:55 +0100

    feat: Day7 slightly more optimized

--- 2024/src/main/kotlin/year2024/day7/Day7.kt
+++ 2024/src/main/kotlin/year2024/day7/Day7.kt
@@ -8,27 +8,25 @@ import kotlin.math.pow
 class Day7 : Solution {
     override fun solveFirst(lines: List<String>): Long {
         return lines.map { it.toEquation() }
-            .filter { isValid(it.first, it.second[0], it.second.withoutFirst()) }
+            .filter { isValid(it.first, it.second) }
             .sumOf { it.first }
     }
 
     override fun solveSecond(lines: List<String>): Long {
         return lines.map { it.toEquation() }
-            .filter { isValid(it.first, it.second[0], it.second.withoutFirst(), true) }
+            .filter { isValid(it.first, it.second, concatenation = true) }
             .sumOf { it.first }
     }
 
     private fun String.toEquation() = Pair(substringBefore(": ").toLong(), substringAfter(": ").split(" ").map(String::toLong))
 
-    fun isValid(target: Long, register: Long, remaining: List<Long>, concatenation: Boolean = false): Boolean {
-        if (remaining.isEmpty()) return register == target
-        if (register > target) return false
-        return isValid(target, register + remaining[0], remaining.withoutFirst(), concatenation)
-                || isValid(target, register * remaining[0], remaining.withoutFirst(), concatenation)
-                || (concatenation && isValid(target, concatenate(register, remaining[0]), remaining.withoutFirst(), true))
+    fun isValid(target: Long, values: List<Long>, workingValue: Long = values.first(), index: Int = 1, concatenation: Boolean = false): Boolean {
+        if (index == values.size) return workingValue == target
+        if (workingValue > target) return false
+        return isValid(target, values, workingValue + values[index], index + 1, concatenation)
+                || isValid(target, values, workingValue * values[index], index + 1, concatenation)
+                || (concatenation && isValid(target, values, concatenate(workingValue, values[index]), index + 1, true))
     }
 
     private fun concatenate(first: Long, second: Long): Long = first * (10.0.pow(floor(log10(second.toDouble())) + 1)).toLong() + second
-
-    private fun List<Long>.withoutFirst() = subList(1, this.size)
 }
